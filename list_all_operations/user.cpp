f3ce173882ff502644e5f8384368bbe8b3693d96
Ivan Keliukh <i.keliukh@samsung.com>
2019-06-13 12:25:23 +0300

    remove inefficient implementation

--- list_all_operations/user.cpp
+++ list_all_operations/user.cpp
@@ -1,7 +1,5 @@
 #define MAX_SIZE	1100000
 
-#define PROPER_IMPLEMENTATION
-#ifdef PROPER_IMPLEMENTATION
 struct Node {
 	int value;
 	int next;
@@ -70,38 +68,4 @@ void dump_list(int copy[])
 		iterator = array[iterator].next;
 	}
 }
-#else
 
-#include <memory.h>
-
-int array[MAX_SIZE];
-unsigned int size;
-
-
-void init()
-{
-	size = 0;
-}
-
-void push_front(int value)
-{
-	memmove(array + 1, array, size * sizeof(int));
-	array[0] = value;
-	size++;
-}
-
-int pop_front()
-{
-	int result = array[0];
-	memmove(array, array + 1, size * sizeof(int));
-	size--;
-	return result;
-}
-
-void dump_list(int copy[])
-{
-	memcpy(copy, array, size * sizeof(int));
-}
-
-
-#endif
