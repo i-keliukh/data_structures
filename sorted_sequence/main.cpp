c443728f97cc31311fe00985c1597abda9c90ca3
Ivan Keliukh <i.keliukh@samsung.com>
2019-06-24 18:15:41 +0300

    add task with sorted array with filter

--- sorted_sequence/main.cpp
+++ sorted_sequence/main.cpp
@@ -6,28 +6,47 @@
 
 
 void init();
-void add_number(int);
-void dump_array(int array[10]);
+void add_number(int value, unsigned short props);
+void dump_array(int array[10], unsigned short mandatory_props);
 
 namespace {
     typedef unsigned int uint;
     static uint seed = 123;
-    uint mrand(uint num) {
-        seed = ((seed * 1103515245) + 12345) & 0x7FFFFFFF;
-        return seed % num;
+    static uint mrand(uint num)
+    {
+        seed = seed * 1103515245 + 37209;
+        return ((seed >> 8) % num);
     }
 
-    std::set<int> numbers;
+    struct StoredNode
+    {
+        int value;
+        unsigned short props;
+        StoredNode(int value, unsigned short props) : value(value), props(props) {}
+        bool operator<(const StoredNode& right) const
+        {
+            return value < right.value;
+        }
+    };
+
+    std::multiset<StoredNode> numbers;
 
     bool test_dump()
     {
         int array[10];
-        dump_array(array);
-
-        int i = 0;
-        for (auto it = numbers.rbegin(); it != numbers.rend(); ++it, ++i)
+        for (int attempts = 0; attempts < 16; attempts++)
         {
-            if (*it != array[i]) return false;
+            unsigned short mandatory_props = mrand(USHRT_MAX + 1);
+            dump_array(array, mandatory_props);
+
+            int i = 0;
+            for (auto it = numbers.rbegin(); it != numbers.rend() && i < 10; ++it)
+            {
+                if ((it->props & mandatory_props) != mandatory_props) continue;
+                if (it->value != array[i])
+                    return false;
+                i++;
+            }
         }
         return true;
     }
@@ -42,13 +61,10 @@ namespace {
         for (int i = 0; i < iterations; ++i)
         {
             int value = mrand(INT_MAX);
-            numbers.insert(value);
-            if (numbers.size() > 10)
-            {
-                numbers.erase(numbers.begin());
-            }
+            unsigned short props = mrand(USHRT_MAX + 1);
+            numbers.insert(StoredNode(value, props));
 
-            add_number(value);
+            add_number(value, props);
 
             if (mrand(1 + iterations / 10) == 0)
             {
--- sorted_sequence/user.cpp
+++ sorted_sequence/user.cpp
@@ -1,46 +1,54 @@
 
-int sorted_array[11];
+struct Element
+{
+    int value;
+    unsigned short props;
+};
+
+Element sorted_array[150000];
 int size;
 
 void init()
 {
     for (int i = 0; i < 10; i++)
     {
-        sorted_array[i] = 0;
+        sorted_array[i].value = 0;
     }
     size = 0;
 }
 
-
-void add_number(int value)
+void add_number(int value, unsigned short props)
 {
     if (size == 0)
     {
-        sorted_array[0] = value;
+        sorted_array[0].value = value;
+        sorted_array[0].props = props;
         size++;
         return;
     }
 
     for (int i = size; i >= 0; i--)
     {
-        if (i > 0 && sorted_array[i - 1] < value)
+        if (i > 0 && sorted_array[i - 1].value < value)
         {
             sorted_array[i] = sorted_array[i - 1];
         }
         else
         {
-            sorted_array[i] = value;
+            sorted_array[i].value = value;
+            sorted_array[i].props = props;
             break;
         }
     }
-
-    if (size < 10) size++;
+    size++;
 }
 
-void dump_array(int array[10])
+void dump_array(int array[10], unsigned short mandatory_props)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0, j = 0; i < size && j < 10; i++)
     {
-        array[i] = sorted_array[i];
+        if ((sorted_array[i].props & mandatory_props) != mandatory_props) continue;
+        array[j] = sorted_array[i].value;
+        j++;
     }
 }
